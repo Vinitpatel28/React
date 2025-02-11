ad390313b6206ee1f303a8692515c2f516748d05
Vinitpatel28 <vinitpatelrajeshbhai@gmail.com>
2025-02-11 20:28:18 +0530

    add Task5 Floder

---
+++ Task5/src/App.jsx
@@ -0,0 +1,12 @@
+import JSXRules from "./components/JSXRules";
+import './App.css'
+function App() {
+  return (
+    <>
+    <div>
+      <JSXRules></JSXRules>
+    </div>
+    </>
+  )
+}
+export default App
